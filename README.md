20bf961d4d9463cd83ac59f2dcb4a1256a073dfd
hrytsiukpavlo <overdrive800392@gmail.com>
2022-10-01 21:57:19 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-bg2zht
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-bg2zht)
---
+++ index.js
@@ -0,0 +1,9 @@
+// Import stylesheets
+import './style.css';
+import store from './store';
+import { bugAdded, bugResolved } from './actions';
+
+store.dispatch(bugAdded('Bug1'));
+store.dispatch(bugResolved(1));
+
+console.log(store.getState());
